2f3613dddfb0c1f87ecbddd19e68dc3f72ff5270
peki555 <52975990+peki555@users.noreply.github.com>
2021-06-14 23:55:44 +0200

    Models project complete. Repository project created with Account Repository

---
+++ BlogLab.Models/Blog/PagedResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Blog
+{
+    // T means it can take any type into class, znao si:)
+    public class PagedResults<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
---
+++ BlogLab.Models/BlogComment/BlogComment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.BlogComment
+{
+    // this will be the comment we load after we create
+    public class BlogComment : BlogCommentCreate
+    {
+        public string Username { get; set; }
+
+        public int ApplicationUserId { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+    }
+}
+
+
+
+
+
